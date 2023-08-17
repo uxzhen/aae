d1bf5e3cdf85ca4f55e5e9a7282f49befcf4c64e
徐振 <2554463433@qq.com>
2023-08-17 13:33:32 +0800

    base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-    base: "/aee/",
+    // base: "/aee/",
     lang: 'zh-CN',
     title: '坦克世界亚服，AEE军团！',
     description: '坦克世界亚服，AEE军团！',
